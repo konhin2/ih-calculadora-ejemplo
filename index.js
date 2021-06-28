e697eea1e3e179443b1433816fef28d589cbcdda
Konhin CM <maskmesta@gmail.com>
2021-06-28 11:05:42 -0500

    boton

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 console.log('hola')
+
+
+// 1. Variables
+const nombre = document.getElementById("capturar-nombre")
+
+const btnSaludar = document.getElementById("btn-saludar")
+
+const areaSaludo = document.getElementById("area-saludo")
+
+// 2. Funciones
+const saludar = (name) => {
+    const saludo = `Hola ${name}!!!`
+    return areaSaludo.innerHTML = saludo
+    
+}
+
+// 3. Eventos
+btnSaludar.addEventListener("click", () => {
+    const nameValue = nombre.value
+    
+    saludar(nameValue)
+}) 
